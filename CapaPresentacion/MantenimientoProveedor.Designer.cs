e568412e961d0293153af5838425820471789c4d
Jason Mijangos <rmijangosherrera@gmail.com>
2022-11-01 18:43:23 -0600

    Renombre de campos interfaz mantenimiento proveedores

--- CapaPresentacion/MantenimientoProveedor.Designer.cs
+++ CapaPresentacion/MantenimientoProveedor.Designer.cs
@@ -30,20 +30,20 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvMantProv = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnMantProvAgregar = new System.Windows.Forms.Button();
+            this.btnMantProvModificar = new System.Windows.Forms.Button();
+            this.btnMantProvEliminar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtMantProvNIT = new System.Windows.Forms.TextBox();
+            this.txtMantProvNomRazSocial = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblMantProvAccError = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMantProv)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.SuspendLayout();
@@ -55,7 +55,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.Controls.Add(this.groupBox3);
             this.panel1.Controls.Add(this.groupBox2);
-            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.dgvMantProv);
             this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
@@ -66,8 +66,8 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtMantProvNomRazSocial);
+            this.groupBox1.Controls.Add(this.txtMantProvNIT);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.ForeColor = System.Drawing.Color.White;
@@ -78,25 +78,25 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos Proveedor";
             // 
-            // dataGridView1
+            // dgvMantProv
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dgvMantProv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(17, 165);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1002, 247);
-            this.dataGridView1.TabIndex = 1;
+            this.dgvMantProv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMantProv.Location = new System.Drawing.Point(17, 165);
+            this.dgvMantProv.Name = "dgvMantProv";
+            this.dgvMantProv.RowHeadersWidth = 51;
+            this.dgvMantProv.RowTemplate.Height = 24;
+            this.dgvMantProv.Size = new System.Drawing.Size(1002, 247);
+            this.dgvMantProv.TabIndex = 1;
             // 
             // groupBox2
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.groupBox2.Controls.Add(this.button3);
-            this.groupBox2.Controls.Add(this.button2);
-            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.btnMantProvEliminar);
+            this.groupBox2.Controls.Add(this.btnMantProvModificar);
+            this.groupBox2.Controls.Add(this.btnMantProvAgregar);
             this.groupBox2.ForeColor = System.Drawing.Color.White;
             this.groupBox2.Location = new System.Drawing.Point(17, 418);
             this.groupBox2.Name = "groupBox2";
@@ -105,35 +105,35 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Acciones";
             // 
-            // button1
+            // btnMantProvAgregar
             // 
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(80, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 30);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Agregar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnMantProvAgregar.ForeColor = System.Drawing.Color.Black;
+            this.btnMantProvAgregar.Location = new System.Drawing.Point(80, 13);
+            this.btnMantProvAgregar.Name = "btnMantProvAgregar";
+            this.btnMantProvAgregar.Size = new System.Drawing.Size(86, 30);
+            this.btnMantProvAgregar.TabIndex = 0;
+            this.btnMantProvAgregar.Text = "Agregar";
+            this.btnMantProvAgregar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnMantProvModificar
             // 
-            this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(173, 13);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 30);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Modificar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnMantProvModificar.ForeColor = System.Drawing.Color.Black;
+            this.btnMantProvModificar.Location = new System.Drawing.Point(173, 13);
+            this.btnMantProvModificar.Name = "btnMantProvModificar";
+            this.btnMantProvModificar.Size = new System.Drawing.Size(86, 30);
+            this.btnMantProvModificar.TabIndex = 1;
+            this.btnMantProvModificar.Text = "Modificar";
+            this.btnMantProvModificar.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnMantProvEliminar
             // 
-            this.button3.ForeColor = System.Drawing.Color.Black;
-            this.button3.Location = new System.Drawing.Point(266, 13);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(86, 30);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Eliminar";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnMantProvEliminar.ForeColor = System.Drawing.Color.Black;
+            this.btnMantProvEliminar.Location = new System.Drawing.Point(266, 13);
+            this.btnMantProvEliminar.Name = "btnMantProvEliminar";
+            this.btnMantProvEliminar.Size = new System.Drawing.Size(86, 30);
+            this.btnMantProvEliminar.TabIndex = 2;
+            this.btnMantProvEliminar.Text = "Eliminar";
+            this.btnMantProvEliminar.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -153,25 +153,25 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre o Razón Social";
             // 
-            // textBox1
+            // txtMantProvNIT
             // 
-            this.textBox1.Location = new System.Drawing.Point(173, 22);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(330, 22);
-            this.textBox1.TabIndex = 2;
+            this.txtMantProvNIT.Location = new System.Drawing.Point(173, 22);
+            this.txtMantProvNIT.Name = "txtMantProvNIT";
+            this.txtMantProvNIT.Size = new System.Drawing.Size(330, 22);
+            this.txtMantProvNIT.TabIndex = 2;
             // 
-            // textBox2
+            // txtMantProvNomRazSocial
             // 
-            this.textBox2.Location = new System.Drawing.Point(173, 53);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(330, 22);
-            this.textBox2.TabIndex = 3;
+            this.txtMantProvNomRazSocial.Location = new System.Drawing.Point(173, 53);
+            this.txtMantProvNomRazSocial.Name = "txtMantProvNomRazSocial";
+            this.txtMantProvNomRazSocial.Size = new System.Drawing.Size(330, 22);
+            this.txtMantProvNomRazSocial.TabIndex = 3;
             // 
             // groupBox3
             // 
             this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.lblMantProvAccError);
             this.groupBox3.ForeColor = System.Drawing.Color.White;
             this.groupBox3.Location = new System.Drawing.Point(527, 419);
             this.groupBox3.Name = "groupBox3";
@@ -180,17 +180,17 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Acciones / Errores";
             // 
-            // label3
+            // lblMantProvAccError
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.lblMantProvAccError.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 19);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "label3";
+            this.lblMantProvAccError.AutoSize = true;
+            this.lblMantProvAccError.Location = new System.Drawing.Point(6, 19);
+            this.lblMantProvAccError.Name = "lblMantProvAccError";
+            this.lblMantProvAccError.Size = new System.Drawing.Size(44, 16);
+            this.lblMantProvAccError.TabIndex = 0;
+            this.lblMantProvAccError.Text = "label3";
             // 
             // MantenimientoProveedor
             // 
@@ -204,7 +204,7 @@
             this.panel1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMantProv)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
@@ -217,15 +217,15 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnMantProvEliminar;
+        private System.Windows.Forms.Button btnMantProvModificar;
+        private System.Windows.Forms.Button btnMantProvAgregar;
+        private System.Windows.Forms.DataGridView dgvMantProv;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtMantProvNomRazSocial;
+        private System.Windows.Forms.TextBox txtMantProvNIT;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMantProvAccError;
     }
 }
