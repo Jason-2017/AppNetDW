a8f4ed552b2b165c936620e468f4df4f056be68f
Jason Mijangos <rmijangosherrera@gmail.com>
2022-11-06 19:47:57 -0600

    Adicion de WS

---
+++ WebServiceHospital/WSHospital.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceHospital
+{
+    /// <summary>
+    /// Descripción breve de WSHospital
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WSHospital : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld(string nitProveedor,
+                                    string codigoPaciente,
+                                    DateTime fechaNacimiento,
+                                    DateTime fechaInicioCobertura)
+        {
+            return "Hola a todos";
+        }
+    }
+}
